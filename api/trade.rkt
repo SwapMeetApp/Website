1024f9f91e11f38f871c4ec4304596a37c4166c2
Vincent Lay <veelay1@gmail.com>
2021-07-07 15:54:48 -0700

    [WIP] add the rest of CRUD for trade api

--- api/trade.rkt
+++ api/trade.rkt
@@ -6,42 +6,77 @@
 (require uuid)
 (require json)
 
-
 ;; trade -> jsexpr?
 (define (trade->jsexpr t)
-    (match t
-     [(trade side1 side2)
-      (hash 'side1 side1
-            'side2 side2)]))  
+  (match t
+    [(trade side1 side2)
+     (hash 'side1 side1
+           'side2 side2)]))  
+
+;; number bytes string -> reponse?
+(define (response/error code message-bytes error-message)
+  (response/full
+   code message-bytes (current-seconds) #"application/json"
+   '()
+   (list (jsexpr->bytes (hash 'message error-message )))))
 
 ;; library -> request? -> any 
 ;; library -> dispatcher/c        
 (define (API library)
- (define-values (trade-dispatch trade-url)
+  (define-values (trade-dispatch trade-url)
     (dispatch-rules
-      [("trade" (string-arg)) #:method "get" (get-trade library)]
-      [("trade") #:method "post" (create-trade library)]))
-      ;; methods rest conventions
- trade-dispatch)
+     [("trade") #:method "post" (create-trade! library)]
+     [("trade" (string-arg)) #:method "get" (get-trade! library)]
+     [("trade" (string-arg)) #:method "put" (update-trade! library)]
+     [("trade" (string-arg)) #:method "delete" (delete-trade! library)]
+     ))
+  trade-dispatch)
 
-;; library -> request String -> any
-(define (get-trade library)
- (lambda (request trade-id)
-;; FIX ME. handle non-uuid strings
-    ;(uuid-string? trade-id)
-       (match (library-find-trade trade-id library)
-        [#false (response/full
- 404 #"Not Found" (current-seconds) #"application/json"
- '()
- (list (jsexpr->bytes (hash 'message "not found"))))]
-        [X (response/jsexpr (trade->jsexpr X))])))
-
-;; library -> request -> any
-(define (create-trade library)
+;; library -> request -> response
+(define (create-trade! library)
   (lambda (request)
     (match (request-post-data/raw request)
-    [#false "fix me"]
-    [body (response/jsexpr "to do")])))
-     ; (match (parse-trade(bytes->jsexpr body))
-                       
+      [#false (response/error 400 #"Bad Request" "empty body")]
+      [body
+       (let ((json (bytes->jsexpr body)))
+         (if 
+          (jsexpr? json) 
+          (match (parse-trade json)
+            [(? string? error-message) (response/error 400 #"Bad Request" error-message)]
+            [trade 
+             (match (library-insert-trade! trade library)
+               [#false (response/error 500 #"Internal Server Error" "Unknown error creating trade")]
+               [id (response/jsexpr id)])])
+          (response/error 400 #"Bad Request" "invalid json")))])))
+
+;; library -> request String -> any
+(define (get-trade! library)
+  (lambda (request trade-id)
+    (match (uuid-string? trade-id)
+      [#false (response/error 400 #"Bad Request" "invalid trade-id")]
+      [#true
+       (match (library-find-trade! trade-id library)
+         [#false (response/error 404 #"Not Found" "not found")]
+         [trade (response/jsexpr (trade->jsexpr trade))])])))
+
+;; library -> request String -> any
+(define (update-trade! library)
+  (lambda (request trade-id)
+    (match (uuid-string? trade-id)
+      [#false (response/error 400 #"Bad Request" "invalid trade-id")]
+      [#true
+       (match (library-update-trade! trade-id library)
+         [#false (response/error 404 #"Not Found" "not found")]
+         [id (response/jsexpr id)])])))
+
+;; library -> request String -> any
+(define (delete-trade! library)
+  (lambda (request trade-id)
+    (match (uuid-string? trade-id)
+      [#false (response/error 400 #"Bad Request" "invalid trade-id")]
+      [#true
+       (match (library-delete-trade! trade-id library)
+         [#false (response/error 404 #"Not Found" "not found")]
+         [id (response/jsexpr id)])])))
+                      
 (provide API)
--- trade.rkt
+++ trade.rkt
@@ -18,50 +18,47 @@
 
 ;; json -> [Or [trade-of uuid] (List Symbol String)]
 (define (parse-trade json)
- (with-handlers ((exn:fail:contract? 
-  (lambda (e) (list 'unexpected-json (format "expect object with side1 and side2: ~a" json)))))
-  (trade
-   (match (hash-ref json 'side1)
-    [(? uuid-string? side1) side1]
-    [ x (list 'not-uuid (format "expected uuid: ~a" x))])
-   (match (hash-ref json 'side2)
-    [(? uuid-string? side2) side2]
-    [ x (list 'not-uuid (format "expected uuid: ~a" x))])))) 
+  (with-handlers ((exn:fail:contract? 
+                   (lambda (e) (list 'unexpected-json (format "expect object with side1 and side2: ~a" json)))))
+    (trade
+     (match (hash-ref json 'side1)
+       [(? uuid-string? side1) side1]
+       [ x (list 'not-uuid (format "expected uuid: ~a" x))])
+     (match (hash-ref json 'side2)
+       [(? uuid-string? side2) side2]
+       [ x (list 'not-uuid (format "expected uuid: ~a" x))])))) 
 
 ;; this is CRUD for a trade
 ;; puts the trade into the database
-;; [Trade-of uuid] -> (void)
+;; [Trade-of uuid] -> [Maybe uuid-string?]
 (define (library-insert-trade! trade library)
- (let ((conn (library-db library)))
-    (call-with-transaction
-     conn 
-     (lambda ()
-       (query-exec conn
-                   "INSERT INTO trades (id, item1, item2) VALUES (?, ?, ?); "
-                   (uuid-string) (trade-side1 trade) (trade-side2 trade))))))
+  (let ((conn (library-db library)))
+    (query-maybe-row conn
+                     "INSERT INTO trades (id, item1, item2) VALUES (?, ?, ?) RETURNING *"
+                     (uuid-string) (trade-side1 trade) (trade-side2 trade))))
 
 ;; String library -> [Maybe trade] 
-(define (library-find-trade trade-id library)
+(define (library-find-trade! trade-id library)
   (let ((conn (library-db library)))
-  (match
-   (query-maybe-row conn
-    "SELECT trades.item1, trades.item2 from trades WHERE ? = trades.id" trade-id)
-    [(vector id item1 item2) 
-      (trade (find-library-book library item1) (find-library-book library item2))]
-    [ _ #false])))
+    (match
+        (query-maybe-row conn
+                         "SELECT trades.item1, trades.item2 from trades WHERE ? = trades.id" trade-id)
+      [(vector id item1 item2) 
+       (trade (find-library-book library item1) (find-library-book library item2))]
+      [ _ #false])))
 
-;; String trade library -> void
-(define (library-update-trade trade-id trade library)
+;; String trade library -> [Maybe uuid-string?]
+(define (library-update-trade! trade-id trade library)
   (let ((conn (library-db library)))
-  (query-exec conn
-              "UPDATE trades SET item1 = ?, item2 = ? WHERE ? = trades.id"
-              (trade-side1 trade) (trade-side2 trade) trade-id)))
+    (query-maybe-row conn
+                "UPDATE trades SET item1 = ?, item2 = ? WHERE ? = trades.id RETURNING id"
+                (trade-side1 trade) (trade-side2 trade) trade-id)))
 
-;; String library -> void
-(define (library-delete-trade trade-id library)
+;; String library -> [Maybe uuid-string?]
+(define (library-delete-trade! trade-id library)
   (let ((conn (library-db library)))
-  (query-exec conn
-              "DELETE FROM trades WHERE ? = trades.id"
-              trade-id)))
+    (query-maybe-row conn
+                     "DELETE FROM trades WHERE ? = trades.id RETURNING id"
+                     trade-id)))
 
 (provide (all-defined-out))
